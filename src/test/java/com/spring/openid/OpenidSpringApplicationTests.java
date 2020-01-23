91344e6a1f329ae05b2d1a9e22e2a0d4399c4532
Jigyasu Garg <jigyasu2010@hotmail.com>
2020-01-23 10:56:05 +0400

    Initial commit

---
+++ src/test/java/com/spring/openid/OpenidSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.openid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenidSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
